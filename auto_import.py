7ca03f99fb4aa3c71823a532203fb2bfcb710039
Daniel <79312811+danieljbk@users.noreply.github.com>
2021-07-26 18:35:12 +0900

    made it work so well

--- auto_import.py
+++ auto_import.py
@@ -2,13 +2,18 @@ import os
 
 
 def auto_import(module):
-    print(f"Automatically installing {module}...\n")
     try:
-        os.system(f'python -m pip install {module}')
-        print(f"Successfully installed {module}.")
+        exec(f"import {module}")
+        return f"import {module}"
     except ImportError:
+        print(f"Automatically installing {module}...\n")
         try:
-            os.system(f'python -m pip3 install {module}')
-            print(f"Successfully installed {module}.\n")
+            os.system(f'python -m pip install {module}')
+            print(f"Successfully installed {module}.")
         except ImportError:
-            print(f"ERROR: Failed to installed {module}.")
+            try:
+                os.system(f'python -m pip3 install {module}')
+                print(f"Successfully installed {module}.\n")
+            except ImportError:
+                print(f"ERROR: Failed to installed {module}.")
+        return f"import {module}"
