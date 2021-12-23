3f953de7e237fc701f09da0284ce2d1075fa4a23
Daniel <79312811+danieljbk@users.noreply.github.com>
2021-12-23 11:08:54 -0500

    Two hours of work

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import smtplib
 from datetime import datetime
 from email.message import EmailMessage
-from auto_import import auto_import  # see auto_import.py for details
+import requests
 exec(auto_import("pyowm"))
 exec(auto_import("schedule"))
 exec(auto_import("uszipcode"))
@@ -11,43 +11,78 @@ exec(auto_import("uszipcode"))
 
 # collect weather info and generate script to send in email
 def weather(location, OPENWEATHERMAP_API_KEY):
-    owm = pyowm.OWM(OPENWEATHERMAP_API_KEY)
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(location)
-    
-    
-    if ", " in location:  # location input was city, country
-        city, country = location.split(", ")
-        state = ""  # No State Data Available
-    else: # location input was zip code
-        search = uszipcode.SearchEngine()
-        location = search.by_zipcode(location)
-        city, country = location.city, "US"  # needed to use "US" for country due to future code determining F˚/C˚
-        state = location.state
-    
-    w = observation.weather
-    
-    detailed_status = w.detailed_status
-    if detailed_status == 'clear sky' or detailed_status == 'thunderstorm':
-        detailed_status = f"a {detailed_status}"
+    API_key = OPENWEATHERMAP_API_KEY
 
-    temp_data = []
-    for x in w.temperature('fahrenheit').items():
-        temp_data.append(x)
-    temp_data = list(
-        map(lambda x: temp_data[temp_data.index(x)][1], temp_data))[:-1]
-    if country != 'US':
-        # convert to Celsius
-        temp_data = list(map(lambda x: (x - 32)/1.8, temp_data))
-    temp_data = list(map(lambda x: int(round(x)), temp_data)
-                     )  # round and get rid of decimals
-    temp, max_temp, min_temp, feels_like = temp_data
+    if ", " in location or len(location) == 5:  # if location is name or zip code
+        owm = pyowm.OWM(OPENWEATHERMAP_API_KEY)
+        mgr = owm.weather_manager()
+        observation = mgr.weather_at_place(location)
+        w = observation.weather
 
-    return city, state, country, detailed_status, w.clouds, w.humidity, temp, max_temp, min_temp, feels_like
+        if ", " in location:  # location input was city, country
+            city, country = location.split(", ")
+        else:  # location input was zip code
+            search = uszipcode.SearchEngine()
+            location = search.by_zipcode(location)
+            # needed to use "US" for country due to future code determining F˚/C˚
+            city, country = location.city, "US"
+
+        detailed_status = w.detailed_status
+        if detailed_status == 'clear sky' or detailed_status == 'thunderstorm':
+            detailed_status = f"a {detailed_status}"
+
+        temp_data = []
+        for x in w.temperature('fahrenheit').items():
+            temp_data.append(x)
+        temp_data = list(
+            map(lambda x: temp_data[temp_data.index(x)][1], temp_data))[:-1]
+        if country != 'US':
+            # convert to Celsius
+            temp_data = list(map(lambda x: (x - 32)/1.8, temp_data))
+        temp_data = list(map(lambda x: int(round(x)), temp_data)
+                         )  # round and get rid of decimals
+        temp, max_temp, min_temp, feels_like = temp_data
+        clouds = w.clouds
+        humidity = w.humidity
+
+    elif len(location) == 7:  # location input was city id
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+        city_id = location
+        Final_url = base_url + "appid=" + API_key + "&id=" + city_id
+        data = requests.get(Final_url).json()
+        weather_data = data['weather']
+        print(data)
+        print()
+
+        city, country = data['name'], data['sys']['country']
+
+        detailed_status = weather_data[0]['description']
+        if detailed_status == 'clear sky' or detailed_status == 'thunderstorm':
+            detailed_status = f"a {detailed_status}"
+
+        temp = data['main']['temp']
+        feels_like = data['main']['feels_like']
+        min_temp = data['main']['temp_min']
+        max_temp = data['main']['temp_max']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        clouds = data['clouds']['all']
+
+        # converting weird temp input to normal Celcius
+        temp_data = [temp, feels_like, min_temp, max_temp]
+        temp_data = list(map(lambda x: x - 273, temp_data))
+        if country == 'US':
+            # convert to Fahrenheit
+            temp_data = list(map(lambda x: x * (9/5) + 32, temp_data))
+
+        temp_data = list(map(lambda x: round(x), temp_data))
+        temp, feels_like, min_temp, max_temp = temp_data
+
+    return city, country, detailed_status, clouds, humidity, temp, max_temp, min_temp, feels_like
 
 
 def script(name, location, type, OPENWEATHERMAP_API_KEY):
-    city, state, country, status, clouds, humidity, temp, max_temp, min_temp, feels_like = weather(
+    city, country, status, clouds, humidity, temp, max_temp, min_temp, feels_like = weather(
         location, OPENWEATHERMAP_API_KEY)
 
     if type == "email":
@@ -65,15 +100,7 @@ def script(name, location, type, OPENWEATHERMAP_API_KEY):
         greeting = "Buenos dias"
         degree_symbol = ' ' + "degrees"
         bot_name = "from Kairos"
-    
-    '''
-    # visually, adding the state was unpretty.
-    # the state is now unused.
-    
-    if state: # if location input was zip code, add state name in script
-        city = city + ", " + state
-    '''
-    
+
     script += f"{greeting} {name}! {hug_emoji}\n"
     script += "\n"
     script += f"{city} is currently experiencing {status} with {clouds}% cloudiness. {cloud_emoji}\n"
@@ -160,23 +187,39 @@ def send():  # send the email
     for data in email_subscribers:
         email_alert(data[0], f"What's the Weather Right Now? ({today()})", script(
             data[1], data[2], "email", OPENWEATHERMAP_API_KEY), GMAIL_USERNAME, GMAIL_API_KEY)
-        
-        if ", " not in data[2]:  # if location input was zip code
+
+        if len(data[2]) == 5:  # if location input was zip code
             search = uszipcode.SearchEngine()
             data[2] = search.by_zipcode(data[2])
             data[2] = data[2].post_office_city  # turn zip code into city, state name
-            
+        elif len(location) == 7:  # location input was city id
+            base_url = "http://api.openweathermap.org/data/2.5/weather?"
+            city_id = data[2]
+            final_url = base_url + "appid=" + API_key + "&id=" + city_id
+            data = requests.get(final_url).json()
+            weather_data = data['weather']
+            city, country = data['name'], data['sys']['country']
+            data[2] = city + ', ' + country
+           
         print("    " + "- sent to:", data[0], "in", data[2])
 
     for data in sms_subscribers:
         email_alert(data[0], f"Weather Update ({today()})", script(
             data[1], data[2], "sms", OPENWEATHERMAP_API_KEY), GMAIL_USERNAME, GMAIL_API_KEY)
-        
-        if ", " not in data[2]:  # if location input was zip code
+
+        if len(data[2]) == 5:  # if location input was zip code
             search = uszipcode.SearchEngine()
             data[2] = search.by_zipcode(data[2])
             data[2] = data[2].post_office_city  # turn zip code into city, state name
-        
+        elif len(location) == 7:  # location input was city id
+            base_url = "http://api.openweathermap.org/data/2.5/weather?"
+            city_id = data[2]
+            final_url = base_url + "appid=" + API_key + "&id=" + city_id
+            data = requests.get(final_url).json()
+            weather_data = data['weather']
+            city, country = data['name'], data['sys']['country']
+            data[2] = city + ', ' + country
+           
         print("    " + "- sent to:", data[0].split("@")[0], "in", data[2])
 
     print(f"\nSuccess: {today()}\n")
